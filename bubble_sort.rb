2403d448da47fcf5a3c3a6ec563f2d8bf12a04c0
JohnReyGimenez <gimenezjr5@gmail.com>
2024-11-05 20:51:32 +0800

    completed bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,22 @@
-
-
-
-module Prints 
-  def prints(sound)
-    print sound
+def bubble_sort(array)
+  loop do
+    swapped = false
+    array.each_with_index do |element,index|
+      break if index == array.length - 1
+    if array[index] > array[index + 1]
+      array[index], array[index + 1] = array[index + 1], array[index] #swap elements
+      swapped = true
+    end
   end
+  break unless swapped
+  end
+  print array
 end
 
+bubble_sort([4,3,78,2,0,2])
 
-class Object 
-  include Prints
-end
 
-New_object = Object.new
-New_object.prints("gay")
+# iterate over the array
+# if the first element is greater than the second elements
+# switch values else go over the next values with the same logic
+# go over the elements again until one everything is skipped over

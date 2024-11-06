ef4ca76fc7e76db792332155c21affc6f5b84a87
JohnReyGimenez <gimenezjr5@gmail.com>
2024-11-06 20:15:38 +0800

    completed number 1

--- basic_OOP.rb
+++ basic_OOP.rb
@@ -3,5 +3,25 @@ class MyCar
     @year = year
     @color = color
     @model = model
+    @speed = 0
+  end
+
+  def speed_up (num)
+    @speed += num
+    "speed has increased by #{num} Kms"
+  end
+  
+  def brake (num)
+    @speed -= num
+    "speed has decreased by #{num} Kms"
+  end
+
+  def shut_off 
+    @speed = 0
+    "car has shut down"
   end
 end
+
+donda = MyCar.new("2001", "black", "new")
+puts donda.shut_off
+puts donda.speed_up(3)

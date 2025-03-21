d25704c284aea2298e507cd9e20c2b994608e46a
JohnReyGimenez <gimenezjr5@gmail.com>
2025-03-21 21:37:19 +0800

    added pseudo code and draft for merge sort

---
+++ recursion/merge_sort.rb
@@ -0,0 +1,14 @@
+def merge_sort(array)
+  if array.length == 1
+    return array
+  else
+    
+  end
+
+
+end
+
+# if only one element, quit
+# else sort left of numbers
+# sort right half of humbers
+# merge sorted halves

3fbe4b90abb73632384e884588ff0f6e2ba76cc9
JohnReyGimenez <gimenezjr5@gmail.com>
2025-03-27 22:07:32 +0800

    rubocop changes

--- recursion/fibonnaci.rb
+++ recursion/fibonnaci.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 def fibs(n)
-  return [0] if n == 0
+  return [0] if n.zero?
   return [0, 1] if n == 1
 
   arr = [0, 1]
-     
+
   until arr.length == n + 1
     next_value = arr[-1] + arr[-2] # accesses the last 2 indices and adds its values
-    arr.push(next_value)   
+    arr.push(next_value)
   end
-  
-  p arr 
-  arr 
+
+  p arr
+  arr
 end
 
 def fibs_rec(n, arr = nil)
-  arr ||= [0, 1]  # Initialize array only on the first call
+  arr ||= [0, 1] # Initialize array only on the first call
 
   puts 'This was printed recursively'
 
-  return (p arr[0, n + 1]) if arr.length > n   # Base case: Stop when enough numbers are generated
+  return (p arr[0, n + 1]) if arr.length > n # Base case: Stop when enough numbers are generated
 
-  arr << arr[-1] + arr[-2]  # Add the next Fibonacci number
+  arr << arr[-1] + arr[-2] # Add the next Fibonacci number
 
-  fibs_rec(n, arr)  # Recursive call with the updated array
+  fibs_rec(n, arr) # Recursive call with the updated array
 end
 
 fibs(8)

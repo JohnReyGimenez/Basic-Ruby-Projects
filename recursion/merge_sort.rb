5b3db62fb8c0bf7e3db72048c9fada532e157487
JohnReyGimenez <gimenezjr5@gmail.com>
2025-03-28 21:33:29 +0800

    merge sort draft

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -2,12 +2,15 @@ def merge_sort(array)
   if array.length == 1
     return array
   else
-    
+    mid = array.length / 2
+    left_half = merge_sort(array.take(mid))   
+    right_half = merge_sort(array.drop(mid)) 
+    p array
   end
-
 
 end
 
+merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
 # if only one element, quit
 # else sort left of numbers
 # sort right half of humbers
